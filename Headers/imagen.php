b4a917da5e95b7176b3e700d5d09931d5a60d989
Dylan1012 <dbds.101203@gmail.com>
2023-09-27 10:15:05 +0200

    Ejercicio imagenes php

---
+++ Headers/imagen.php
@@ -0,0 +1,23 @@
+<?php
+
+$letMeIn = $_GET['letMeIn'] ?? 0;
+$img = $_GET['img'] ?? "si2";
+
+if ($letMeIn == 0) {
+    header('Content-type: image/png');
+    imagepng(imagecreatefrompng("/home/alumno/projects/Carpeta-Oculta/no.png"));
+} else if ($letMeIn == 1) {
+    if (file_exists("/home/alumno/projects/Carpeta-Oculta/$img.jpg")) {
+        header('Content-type: image/png');
+        imagepng(imagecreatefromjpeg("/home/alumno/projects/Carpeta-Oculta/$img.jpg"));
+    } else {
+        http_response_code(404);
+        header('Content-type: image/png');
+        imagepng(imagecreatefrompng("/home/alumno/projects/Carpeta-Oculta/404.png"));
+    }
+} else {
+    header('Content-type: image/png');
+    imagepng(imagecreatefrompng("/home/alumno/projects/Carpeta-Oculta/no.png"));
+}
+
+?>
